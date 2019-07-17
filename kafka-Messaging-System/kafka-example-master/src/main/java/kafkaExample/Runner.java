92d996ad52e8f589617d4e761b15cfbe4fe2f9a6
Satya Mamidi <52873343+yesmamidi@users.noreply.github.com>
2019-07-16 22:49:11 -0400

    messaging system implemented

---
+++ kafka-Messaging-System/kafka-example-master/src/main/java/kafkaExample/Runner.java
@@ -0,0 +1,15 @@
+package kafkaExample;
+
+public class Runner {
+    public static void main(String[] args) throws InterruptedException {
+        if (args[0].equals("producer")) {
+            Producer.main(args);
+        } else if (args[0].equals("consumer")) {
+            Consumer.main(args);
+        } else if (args[0].equals("stream")) {
+            Stream.main(args);
+        } else {
+            throw new IllegalArgumentException("Don't know how to do " + args[0]);
+        }
+    }
+}
